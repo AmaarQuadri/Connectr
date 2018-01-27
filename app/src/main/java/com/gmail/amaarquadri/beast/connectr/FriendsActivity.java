8dcada99698c00168f05504c384bd83a49b94d4b
Amanda Morin <asmmorin@edu.uwaterloo.ca>
2018-01-27 16:47:40 -0500

    Changing to test

--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/FriendsActivity.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/FriendsActivity.java
@@ -1,9 +1,16 @@
 package com.gmail.amaarquadri.beast.connectr;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * Created by amandamorin on 2018-01-27.
@@ -18,9 +25,23 @@ public class FriendsActivity extends Activity {
         User user = (User) getIntent().getSerializableExtra("user");
         friendsLinearLayout = findViewById(R.id.friends_list);
 
+        ArrayList<User> friends = user.getFriends();
+        friends.sort(Comparator.comparing(User::getUsername));
         for (User friend : user.getFriends()) {
             LinearLayout friendRow = new LinearLayout(this);
             friendRow.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView friendNameTextView = new TextView(this);
+            friendNameTextView.setText(friend.getUsername());
+            friendNameTextView.setTextSize(16);
+            //TODO: make sure set color works
+            friendNameTextView.setTextColor(0);
+            friendRow.addView(friendNameTextView);
+
+            ToggleButton button = new ToggleButton(this);
+            button.setOnClickListener(view -> {
+
+            });
 
         }
     }
