36addbba0a8396d92e24a64464b825aa9fed5ae5
Amanda Morin <asmmorin@edu.uwaterloo.ca>
2018-01-27 18:35:08 -0500

    Changing to test

--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/MainActivity.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/MainActivity.java
@@ -1,10 +1,34 @@
 package com.gmail.amaarquadri.beast.connectr.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.gmail.amaarquadri.beast.connectr.R;
 
 /**
  * Created by amaar on 2018-01-27.
  */
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+
+
+    }
+
+    public void logout(View view) {
+    }
+
+    public void addFriend(View view) {
+    }
+
+    public void friendsPage(View view) {
+    }
+
+    public void findFriend(View view) {
+    }
 }
