23cfa3675bc209bf37f47b1582c57301c5237baf
Amaar Quadri <amaarquadri@gmail.com>
2018-01-27 18:18:03 -0500

    Added stuffs

--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/SignupPageActivity.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/SignupPageActivity.java
@@ -1,10 +1,25 @@
 package com.gmail.amaarquadri.beast.connectr.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.gmail.amaarquadri.beast.connectr.R;
 
 /**
  * Created by amaar on 2018-01-27.
  */
 
 public class SignupPageActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+
+
+    }
+
+    public void create(View view) {
+    }
 }
