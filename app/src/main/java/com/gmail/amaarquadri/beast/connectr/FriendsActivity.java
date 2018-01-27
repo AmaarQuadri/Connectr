63478b7f4be0d15968063e85b68e1fcbcd0d0508
Amanda Morin <asmmorin@edu.uwaterloo.ca>
2018-01-27 16:00:10 -0500

    Changing to test

---
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/FriendsActivity.java
@@ -0,0 +1,10 @@
+package com.gmail.amaarquadri.beast.connectr;
+
+import android.app.Activity;
+
+/**
+ * Created by amandamorin on 2018-01-27.
+ */
+
+public class FriendsActivity extends Activity {
+}
--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/ServerResponse.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ServerResponse.java
@@ -7,5 +7,28 @@ import java.io.Serializable;
  */
 
 public class ServerResponse implements Serializable {
+    public enum Type {
+        LOGIN_RESULT, LOGIN_FAILED
+    }
 
+    private final Type type;
+    private final User user;
+
+    public ServerResponse() {
+        type = Type.LOGIN_FAILED;
+        user = null;
+    }
+
+    public ServerResponse(User user) {
+        type = Type.LOGIN_RESULT;
+        this.user = user;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
