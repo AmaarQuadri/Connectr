95daade1014df9b36dea8ad085472f901dd2f4d7
Amanda Morin <asmmorin@edu.uwaterloo.ca>
2018-01-28 04:13:14 -0500

    updaste

---
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/ViewLocationRequestsActivity.java
@@ -0,0 +1,11 @@
+package com.gmail.amaarquadri.beast.connectr.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by amandamorin on 2018-01-28.
+ */
+
+public class ViewLocationRequestsActivity extends Activity {
+
+}
