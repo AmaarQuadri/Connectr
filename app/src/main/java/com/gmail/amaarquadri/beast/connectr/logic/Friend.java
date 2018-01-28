28722c855d4ed55315a280bbdfd45f4b9ac36380
Amaar Quadri <amaarquadri@gmail.com>
2018-01-27 23:04:48 -0500

    Added stuffs

--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/logic/Friend.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/logic/Friend.java
@@ -3,15 +3,22 @@ package com.gmail.amaarquadri.beast.connectr.logic;
 /**
  * Created by amandamorin on 2018-01-27.
  */
-
 public class Friend extends User {
-    private final boolean iHavePermission;
-    private final boolean friendHasPermission;
+    private boolean iHavePermission;
+    private boolean friendHasPermission;
 
     public Friend(int index, int id, String username, String password, LocationData lastLocation,
                   boolean iHavePermission, boolean friendHasPermission) {
         super(index, id, username, password, lastLocation, null);
         this.iHavePermission = iHavePermission;
+        this.friendHasPermission = friendHasPermission;
+    }
+
+    public void setiHavePermission(boolean iHavePermission) {
+        this.iHavePermission = iHavePermission;
+    }
+
+    public void setFriendHasPermission(boolean friendHasPermission) {
         this.friendHasPermission = friendHasPermission;
     }
 
