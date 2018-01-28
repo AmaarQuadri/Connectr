cac6c9f2f2bfcabec01193977bdc414c3f4be075
Amanda Morin <asmmorin@edu.uwaterloo.ca>
2018-01-28 03:15:10 -0500

    updaste

---
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/requestLocationActivity.java
@@ -0,0 +1,11 @@
+package com.gmail.amaarquadri.beast.connectr.ui;
+
+import android.app.Activity;
+
+/**
+ * Created by amandamorin on 2018-01-28.
+ */
+
+public class requestLocationActivity extends Activity {
+    //requests location from another user. however. requires some kind of pop up from said user.
+}
