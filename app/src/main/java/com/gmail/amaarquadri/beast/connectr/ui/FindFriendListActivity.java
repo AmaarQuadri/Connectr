7285514e41a54d30359eb07fff4481fed741e63a
Amaar Quadri <amaarquadri@gmail.com>
2018-01-27 23:53:02 -0500

    Added stuffs

--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/FindFriendListActivity.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/FindFriendListActivity.java
@@ -3,6 +3,8 @@ package com.gmail.amaarquadri.beast.connectr.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.gmail.amaarquadri.beast.connectr.R;
 import com.gmail.amaarquadri.beast.connectr.logic.Friend;
@@ -12,15 +14,23 @@ import com.gmail.amaarquadri.beast.connectr.logic.User;
  * Created by amandamorin on 2018-01-27.
  */
 public class FindFriendListActivity extends Activity {
+    private LinearLayout sharingListLinearLayout;
     private User user;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_friend_list);
+
+        sharingListLinearLayout = findViewById(R.id.sharing_list);
         user = (User) getIntent().getSerializableExtra("user");
 
         for (Friend friend : user.getFriends()) if (friend.iHavePermission()) {
+            LinearLayout friendRow = new LinearLayout(this);
+            friendRow.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView textView = new TextView(this);
+            textView.setText(friend.getUsername());
 
         }
     }
