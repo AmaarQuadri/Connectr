a4cf37d17807e6cd3d38432b0cdb3c8f0b993a50
tdog21 <31562272+tdog21@users.noreply.github.com>
2018-01-28 00:45:39 -0500

    fuk u wolfram

--- app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/UpdateLocationService.java
+++ app/src/main/java/com/gmail/amaarquadri/beast/connectr/ui/UpdateLocationService.java
@@ -7,23 +7,27 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by amaar on 2018-01-27.
  */
 
 public class UpdateLocationService extends Service {
-    //private FusedLocationProviderClient mFusedLocationClient
+    //
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
         return null;
     }
-
-
 
     private Location getLocation() {
         return null;
     }
+    mFusedLocationClient.getLastLocation()
 }
